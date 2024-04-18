186b3f05eb53dfba92f534a37d132c21f70a7f05
Raphael Bonaccorsi <81281352+RaphaelBonaccorsi@users.noreply.github.com>
2024-04-18 19:31:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,80 +1,107 @@
 def receive_input():
     numberOfStates = receive_numberOfStates()
     numberOfTerminalSymbols, terminalSymbols = receive_terminalSymbols()
+    numberOfAcceptanceStages, AcceptanceStages = receive_AcceptanceStates()
+    numberOfTransitions = receive_Transitions()
+    transitions = receive_InitialFinal_Transitions()
+    numberOfChains = receive_numberOfChains()
+    chains = receive_Chains(numberOfChains)
 
-def receive_numberOfStates():
+def receive_numberOfStates(): # First input, number of states
     while True:
         numberOfStates = input() # Number of states, first input
         try:
-            int(numberOfStates)
+            numberOfStates = int(numberOfStates)
+            if int(numberOfStates) >= 1 and int(numberOfStates) <= 10:
+                numberOfStates = int(numberOfStates)
+                break
+            else:
+                print("Number of states should be between 1 and 10\nPlease, try again.")
+                continue
         except:
             print("Number of states should be a number\nPlease, try again.")
             continue
 
-        if int(numberOfStates) >= 1 and int(numberOfStates) <= 10:
-            numberOfStates = int(numberOfStates)
-            break
-        else:
-            print("Number of states should be between 1 and 10\nPlease, try again.")
-            continue
-
     return numberOfStates
 
-def receive_terminalSymbols(): # Criar try except para verificar se o primeiro input é numero (semelhante a função acima), criar casos de teste
+def receive_terminalSymbols(): # Second input, number of terminal symbols and terminal symbols
     while True:
         terminalSymbols = input().split() # Receive the terminal symbols
         try:
             numberOfTerminalSymbols = int(terminalSymbols[0]) # Get the number of terminal symbols
+            terminalSymbols = terminalSymbols[1:] # Remove the first element of the list
+            if len(terminalSymbols) > 10:
+                print("Too much terminal symbols")
+                continue
+            else:
+                break
         except:
             print("Number of terminal symbols should be a number\nPlease, try again.")
             continue
-        terminalSymbols = terminalSymbols[1:] # Remove the first element of the list
-        if len(terminalSymbols) > 10:
-            print("Too much terminal symbols")
-            continue
+        
+        
     return numberOfTerminalSymbols, terminalSymbols
 
+def receive_AcceptanceStates():# Third input, number of acceptance states and acceptance states
+    inputStages = input()
+    try:
+        AcceptanceStages = inputStages.split()
 
-def receive_AcceptanceStates():# PRECISA FAZER O TESTE 
-        inputStages = input()
+        numberOfAcceptanceStages = int(AcceptanceStages[0])
+        AcceptanceStages = AcceptanceStages[1:]
+
+        for i in range(len(AcceptanceStages)):
+            AcceptanceStages[i] = int(AcceptanceStages[i])
+
+        return numberOfAcceptanceStages, AcceptanceStages
+    except Exception as e:
+        return e
+            
+def receive_Transitions(): # Fourth input, number of transitions
+    while True:
+        numberOfTransitions = input()
         try:
-            AcceptanceStages = inputStages.split()
+            numberOfTransitions = int(numberOfTransitions)
+            if numberOfTransitions > 50 and numberOfTransitions < 1:
+                print("Invalid transitions")
+                continue
+            else:
+                break
+        except:
+            print("Number of transitions should be a number\nPlease, try again.")
+            continue
 
-            numberOfAcceptanceStages = int(AcceptanceStages[0])
-            AcceptanceStages = AcceptanceStages[1:]
-
-            for i in range(len(AcceptanceStages)):
-                AcceptanceStages[i] = int(AcceptanceStages[i])
-            
-        
-            return numberOfAcceptanceStages, AcceptanceStages
-        except Exception as e:
-            return e
-            
-def receive_Transitions():
-    numberOfTransitions = int(input())
     return numberOfTransitions
 
-def receive_InitialFinal_Transitions():
+def receive_InitialFinal_Transitions(): # Fifth input, transitions
     transitions = []
     for i in range(numberOfTransitions):
-        transitions = input(format(i+1))
-        transitions.append(transitions.split())
+        transition = input()
+        transitions.append(transition)
     return transitions
 
-def receive_numberOfChains():
-    numberOfChains = int(input())
-    if(numberOfChains > 10):
-        print("too much chains")
+def receive_numberOfChains(): # Number of chains
+    while True:
+        numberOfChains = input()
+        try:
+            numberOfChains = int(numberOfChains)
+            if numberOfChains > 10:
+                print("Too much chains")
+                continue
+            else:
+                break
+        except:
+            print("Number of chains should be a number\nPlease, try again.")
+            continue
     return numberOfChains
 
-def receive_Chains():
+def receive_Chains(numberOfChains): # Receive chains
     chains = []
-    for i in range(receive_numberOfChains):
-        chain = input(format(i+1))
-        while len(chain) > 20:
-            print("Chain should have at most 20 characters.")
-            chain = input(format(i+1))
+    for i in range(numberOfChains):
+        chain = input()
+        if len(chain) > 20:
+            print("Chain too long")
+            exit(0)
         chains.append(chain)
     return chains
  
